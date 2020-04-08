9cb70a4f0084f8d8e5285d788aa7ed8e96958c33
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-08 14:35:50 +0800

    article CRUD 基本完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:edit, :update, :destroy, :show]
+
   def index
     @articles = Article.all
   end
+
   def new
     @article = Article.new
   end
@@ -15,8 +19,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_clear)
+      redirect_to articles_path, notice: "已修改文章!"    
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "已刪除文章!"
+  end
+
   private
   def article_clear 
     params.require(:article).permit(:title, :content)
+  end
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
