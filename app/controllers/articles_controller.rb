008e3c12e960651554c08964798aa00bea9bbf9b
Lawa0921 <bag571ivy3470@gmail.com>
2020-04-08 14:06:43 +0800

    article 新增 new 功能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,21 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_clear)
+    if @article.save
+      redirect_to articles_path, notice: "已發布消息!"    
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_clear 
+    params.require(:article).permit(:title, :content)
+  end
 end
